4d9195b909debea692b1075bc62f9de4e1888616
Nastya Gushchina <gushina.ae@yandex.ru>
2024-11-13 21:53:17 +0300

    The files <calculator.py> and <triangle.py>  have been changed. Added tests

--- calculate.py
+++ calculate.py
@@ -1,33 +1,53 @@
 import circle
 import square
+import triangle
 
-
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+    "area-circle": 1,
+    "perimeter-circle": 1,
+    "area-square": 1,
+    "perimeter-square": 1,
+    "area-triangle": 3,
+    "perimeter-triangle": 3
+}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    assert fig in figs
+    assert func in funcs
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    if any(s <= 0 for s in size):
+        raise ValueError("The dimensions of the figure must be greater than zero")
+
+    expected_size = sizes.get(f"{func}-{fig}")
+    if len(size) != expected_size:
+        raise ValueError(f"{expected_size} parameters are expected for {fig} and the function {func}")
+
+    if fig == 'triangle':
+        if size[0] + size[1] <= size[2] or size[0] + size[2] <= size[1] or size[1] + size[2] <= size[0]:
+            raise ValueError("Incorrect dimensions for the triangle")
+
+    result = eval(f"{fig}.{func}(*{size})")
+    return result
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
+    func = ''
+    fig = ''
+    size = list()
 
+    while fig not in figs:
+        fig = input(f"Enter figure name, avaliable are {figs}:\n")
 
+    while func not in funcs:
+        func = input(f"Enter function name, avaliable are {funcs}:\n")
 
+    while True:
+        size = list(map(int, input(f"Input {sizes.get(f'{func}-{fig}', 1)} figure sizes separated by space: \n").split(' ')))
+        try:
+            result = calc(fig, func, size)
+            break
+        except ValueError as e:
+            print(e)
+
+    print(f'{func} of {fig} is {result}')
---
+++ module/test_calculate.py
@@ -0,0 +1,210 @@
+import unittest
+import math
+import sys
+sys.path.append("/Документы/geometric_lib")
+from calculate import calc
+
+class TestCalculate_CircleArea(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "circle"
+        func = "area"
+        size = [5]
+        # Act
+        expected_result = math.pi * size[0] * size[0] 
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "circle"
+        func = "area"
+        size = [-5]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "circle"
+        func = "area"
+        size = []
+        # Act
+        expected_result ="1 parameters are expected for circle and the function area"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+class TestCalculate_CirclePerimeter(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "circle"
+        func = "perimeter"
+        size = [5]
+        # Act
+        expected_result = 2 * math.pi * size[0]
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "circle"
+        func = "perimeter"
+        size = [-5]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+    
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "circle"
+        func = "perimeter"
+        size = []
+        # Act
+        expected_result ="1 parameters are expected for circle and the function perimeter"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+class TestCalculate_SquareArea(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "square"
+        func = "area"
+        size = [25]
+        # Act
+        expected_result = size[0] * size[0]
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "square"
+        func = "area"
+        size = [-25]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+    
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "square"
+        func = "area"
+        size = []
+        # Act
+        expected_result ="1 parameters are expected for square and the function area"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+class TestCalculate_SquarePerimeter(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "square"
+        func = "perimeter"
+        size = [25]
+        # Act
+        expected_result = 4 * size[0]
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "square"
+        func = "perimeter"
+        size = [-25]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "square"
+        func = "area"
+        size = []
+        # Act
+        expected_result ="1 parameters are expected for square and the function area"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+class TestCalculate_TriangleArea(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "area"
+        size = [3, 4, 5]
+        # Act
+        p = (size[0] + size[1] + size[2])/2
+        expected_result = math.sqrt(p * (p - size[0])*(p - size[1])*(p - size[2]))
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "area"
+        size = [3, 4, -5]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "area"
+        size = [3, 4]
+        # Act
+        expected_result ="3 parameters are expected for triangle and the function area"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+
+class TestCalculate_TrianglePerimeter(unittest.TestCase):
+    def test_valid_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "perimeter"
+        size = [3, 4, 5]
+        # Act
+        expected_result =  size[0] + size[1] + size[2]
+        result = calc(fig, func, size)
+        # Assert
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_invalid_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "perimeter"
+        size = [3, 4, -5]
+        # Act
+        expected_result ="The dimensions of the figure must be greater than zero"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
+    
+    def test_not_enough_values(self):
+        # Arrange
+        fig = "triangle"
+        func = "perimeter"
+        size = [3, 4]
+        # Act
+        expected_result ="3 parameters are expected for triangle and the function perimeter"
+        # Assert
+        with self.assertRaisesRegex(ValueError, expected_result):
+            calc(fig, func, size)
